fa9cc700c31d3ea65826e7fd4393999f32223708
ki9-feb11 <kara.kiku0211@gmial.com>
2025-06-04 08:34:02 +0000

    [Finish]9a

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,6 +8,9 @@ class BooksController < ApplicationController
 
   def show
     @book = Book.find(params[:id])
+    unless ViewCount.where(created_at: Time.zone.now.all_day).find_by(user_id: current_user.id, book_id: @book.id)
+      current_user.view_counts.create(book_id: @book.id)
+    end
     @book_new = Book.new
     @book_comment = BookComment.new
   end
